bdc113b76844cccd3a8e9d175d6bbaab59682188
bosung si <qweasdzxc0311@naver.com>
2024-10-10 23:38:31 +0900

    countDown demo 생성

---
+++ resources/www/docs1/demo/countdown.blade.php
@@ -0,0 +1,9 @@
+<x-www-app>
+    <x-countdown countdownDate="2024/10/11 23:10:00" size="h2"></x-countdown>
+    <x-countdown-day countdownDate="2024/10/11 23:10:00" size="h4"></x-countdown-day>
+    <x-countdown-hour countdownDate="2024/10/11 23:10:00" size="h4"></x-countdown-hour>
+    <x-countdown-card countdownDate="2024/10/11 23:10:00" size="h4"></x-countdown-card>
+    <!-- tertiary, secondary 조금 연한거 차이  -->
+    <x-countdown-card-day countdownDate="2024/10/11 23:10:00" size="h3" backGround="tertiary"></x-countdown-card-day>
+    <x-countdown-card-hour countdownDate="2024/10/11 23:10:00" size="h2" backGround="secondary"></x-countdown-card-hour>
+</x-www-app>
